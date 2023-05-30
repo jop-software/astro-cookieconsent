e45089db5335c41ec0dc1122dde117fec14c816e
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2023-05-30 22:19:25 +0200

    task: setup GHA

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import * as CookieConsent from "vanilla-cookieconsent";
 
 const createPlugin = (config: UserConfig) => {
-	return {
-		name: "@jop-software/astro-cookie-consent",
-		hooks: {
-			"astro:config:setup": async ({ injectScript }: any) => {
-				injectScript("page", "import 'vanilla-cookieconsent'; import 'vanilla-cookieconsent/dist/cookieconsent.css';");
-                injectScript("page", `window.cookieConentConfiguration = ${JSON.stringify(config)}`);
-				injectScript("page", `window.cookieConent = initCookieConsent();`)
-				injectScript("page", `window.cookieConent.run(window.cookieConentConfiguration);`)
-			},
-		},
-	};
+  return {
+    name: "@jop-software/astro-cookie-consent",
+    hooks: {
+      "astro:config:setup": async ({ injectScript }: any) => {
+        injectScript(
+          "page",
+          "import 'vanilla-cookieconsent'; import 'vanilla-cookieconsent/dist/cookieconsent.css';"
+        );
+        injectScript(
+          "page",
+          `window.cookieConentConfiguration = ${JSON.stringify(config)}`
+        );
+        injectScript("page", `window.cookieConent = initCookieConsent();`);
+        injectScript(
+          "page",
+          `window.cookieConent.run(window.cookieConentConfiguration);`
+        );
+      },
+    },
+  };
 };
 
 export type { CookieConsent };
